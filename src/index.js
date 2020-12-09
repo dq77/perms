b5c97d336265feb0ef7f5430617557ac1aa53154
掉漆 <270778319@qq.com>
2020-12-09 19:04:35 +0800

    添加历史记录功能

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ bigImg.width="1297";
 document.getElementById('logo-area').appendChild(bigImg)
 
 let artVal = 0 // 缓存当前文章id 避免编辑文章且未保存时改动id
+let historyList = [] // 历史记录
 console.log(process.env.NODE_ENV);
 // 点击按钮查询
 document.querySelector('.searchbtn').addEventListener('click', ()=> {
@@ -39,6 +40,20 @@ document.querySelector('.searchbtn').addEventListener('click', ()=> {
   } else {
     tips('请输入笔记编码', 'red')
   }
+})
+// 查询历史记录
+document.querySelector('.hisbtn').addEventListener('click', ()=> {
+  if (document.querySelector('.titleipt').value) {
+    artVal = document.querySelector('.titleipt').value
+    readHistory(artVal)
+  } else {
+    tips('请输入笔记编码', 'red')
+  }
+})
+
+// 隐藏历史记录
+document.querySelector('.clsbtn').addEventListener('click', ()=> {
+  document.querySelector('.hismodel').className = 'hismodel'
 })
 // 回车查询
 document.querySelector('.titleipt').addEventListener("keypress", (e) => {
@@ -79,6 +94,36 @@ document.querySelector('.w-e-text').addEventListener("keydown", (e) => {
     }
   }
 }, true);
+
+// 获取历史口
+function readHistory(id) {
+  axios.post('/picert/user/gethistory', {
+    artid: id
+  }).then(function (res) {
+    if (res.data && res.data[0]) {
+      historyList = res.data
+      let str = ''
+      for (let item of res.data) {
+        str += `<tr><td>${item.val}</td><td>${item.savetime}</td><td data-idx="${item.id}" class="seehis">查看</td></tr>`
+      }
+      document.querySelector('#histbody').innerHTML = str
+      document.querySelector('.hismodel').className = 'hismodel showmdl'
+      // 查看历史按钮
+      const btns = document.querySelectorAll('.seehis')
+      for (let i = 0; i < btns.length;i++) {
+        btns[i].addEventListener('click', (e)=> {
+          const slct = historyList.find((item) => {
+            return item.id === e.target.dataset.idx-0
+          })
+          editor.txt.html(slct.val)
+          document.querySelector('.hismodel').className = 'hismodel'
+        })
+      }
+    } else if (res.data.length === 0) {
+      tips('该笔记编码并没有历史记录', 'red')
+    }
+  })
+}
 
 // 获取文章详情接口
 function readVal(id) {
